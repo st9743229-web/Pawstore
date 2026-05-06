90be49de1a5f2d2ef658d15bccdbe2275a1d77eb
sofiia_lazutchik <st9743229@stud.duikt.edu.ua>
2026-05-06 08:44:04 +0300

    Add jobs data structure, images and styles

---
+++ jobs.js
@@ -0,0 +1,18 @@
+const jobs = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    company: "Google",
+    salary: "1000$",
+    location: "Remote"
+  },
+  {
+    id: 2,
+    title: "HR Manager",
+    company: "Huawei",
+    salary: "800$",
+    location: "Kyiv"
+  }
+];
+
+export default jobs;
